ec6db052b9ac086cff4b15ce16d797e26df88237
DiegoLudena <d_l_m88@yahoo.es>
2023-01-24 13:12:42 +0100

    Creacion del menu y la opcion de rectangulo

--- src/ejerciciofiguras/ClasePrincipal.java
+++ src/ejerciciofiguras/ClasePrincipal.java
@@ -1,9 +1,49 @@
 package ejerciciofiguras;
 
+import java.util.Scanner;
+
 public class ClasePrincipal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		menu();
+		
+		Scanner scanner = new Scanner(System.in);
+		int opcion = scanner.nextInt();
+		
+		switch (opcion) {
+		case 1 :
+			break;
+		case 2:
+			System.out.println("Indica el color del rectángulo:");
+			String color = scanner.next();
+			System.out.println("Indica la longitud de la base");
+			double base = scanner.nextDouble();
+			System.out.println("Indica la longitud de la altura");
+			double altura = scanner.nextDouble();
+			
+			
+			Rectangulo miRectangulo = new Rectangulo("Rectangulo", color, base, altura);
+			if (miRectangulo.comprobarCuadrado() == true){
+				miRectangulo.setNombre("Cuadrado");
+				System.out.println("Es un " +miRectangulo.getNombre());
+			}
+			else {
+				System.out.println("Es un " +miRectangulo.getNombre());
+			}
+			System.out.println("El área es " +miRectangulo.getArea());
+			System.out.println("El perímetro es " +miRectangulo.getPerimetro());
+					
+			
+			
+			break;
+		default: 
+			System.out.println("Opción inválida");
+			menu();
+		}
+	}
+	public static void menu() {
+		System.out.println("   FIGURAS PLANAS \n----------------------");
+		System.out.println("1. Crear un triángulo \n2. Crear un rectángulo \n\n Escoge una opción:");
 
 	}
 
--- src/ejerciciofiguras/Rectangulo.java
+++ src/ejerciciofiguras/Rectangulo.java
@@ -4,10 +4,12 @@ public class Rectangulo extends Figura{
 	private double base;
 	private double altura;
 	
-	public Rectangulo(String nombre, String color, double perimetro, double area, double base, double altura) {
-		super(nombre, color, perimetro, area);
+	
+	public Rectangulo(String nombre, String color, double base, double altura) {
+		super(nombre, color,2*base+2*altura, base*altura);
 		this.base = base;
 		this.altura = altura;
+		
 		}
 
 	public double getBase() {
@@ -17,13 +19,21 @@ public class Rectangulo extends Figura{
 	public void setBase(double base) {
 		this.base = base;
 	}
-
+	
 	public double getAltura() {
 		return altura;
 	}
 
 	public void setAltura(double altura) {
 		this.altura = altura;
+	}
+	
+	public void setPerimetro(double perimetro) {
+		this.perimetro = 2*base+2*altura;
+	}
+	
+	public void setArea(double area) {
+		this.area = base*altura;
 	}
 	
 	public boolean comprobarCuadrado() {
