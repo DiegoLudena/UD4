1520d39cd27fe3b0b9a580beb1f4135c11c748ef
DiegoLudena <d_l_m88@yahoo.es>
2023-01-25 12:59:36 +0100

    Exception letras en los menus

--- src/ejerciciofiguras/ClasePrincipal.java
+++ src/ejerciciofiguras/ClasePrincipal.java
@@ -1,6 +1,7 @@
 package ejerciciofiguras;
 
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class ClasePrincipal {
 
@@ -8,43 +9,55 @@ public class ClasePrincipal {
 		menu();
 	}
 		public static void menu() {
+			
 			System.out.println("   FIGURAS PLANAS \n----------------------");
 			System.out.println("1. Crear un triángulo \n2. Crear un rectángulo \n\n Escoge una opción:");
 		
-		Scanner scanner = new Scanner(System.in);
-		int opcion = scanner.nextInt();
+			Scanner scanner = new Scanner(System.in);
+			int opcion = scanner.nextInt();
 		
-		switch (opcion) {
-		case 1 :
-			Triangulo miTriangulo = new Triangulo ("Triangulo", "Amarillo", 8, 8, 8, 6.93);
-			miTriangulo.dime_caracteristicas();
-			miTriangulo.tipoTriangulo();
-			break;
-		case 2:
-			System.out.println("Indica el color del rectángulo:");
-			String color = scanner.next();
-			System.out.println("Indica la longitud de la base");
-			double base = scanner.nextDouble();
-			System.out.println("Indica la longitud de la altura");
-			double altura = scanner.nextDouble();
-			
-			
-			Rectangulo miRectangulo = new Rectangulo("Rectangulo", color, base, altura);
-			if (miRectangulo.comprobarCuadrado() == true){
-				miRectangulo.setNombre("Cuadrado");
-				System.out.println("Es un " +miRectangulo.getNombre());
+			try {
+				switch (opcion) {
+				case 1 :
+					Triangulo miTriangulo = new Triangulo ("Triangulo", "Amarillo", 8, 8, 8, 6.93);
+					miTriangulo.dime_caracteristicas();
+					miTriangulo.tipoTriangulo();
+					break;
+				case 2:
+					try {
+						System.out.println("Indica el color del rectángulo:");
+						String color = scanner.next();
+						System.out.println("Indica la longitud de la base");
+						double base = scanner.nextDouble();
+						
+						System.out.println("Indica la longitud de la altura");
+						double altura = scanner.nextDouble();
+						
+						
+						Rectangulo miRectangulo = new Rectangulo("Rectangulo", color, base, altura);
+						if (miRectangulo.comprobarCuadrado() == true){
+							miRectangulo.setNombre("Cuadrado");
+							System.out.println("Es un " +miRectangulo.getNombre());
+						}
+						else {
+							System.out.println("Es un " +miRectangulo.getNombre());
+						}
+						System.out.println("El área es " +miRectangulo.getArea() +"cm\u00B2.");
+						System.out.println("El perímetro es " +miRectangulo.getPerimetro() +"cm.");		
+						break;
+					} catch (InputMismatchException e){
+						System.out.println("No has introducido un numero. \nPorfavor introduzca un numero");
+						menu();
+					}
+					
+				default: 
+					System.out.println("Opción inválida");
+					menu();
+				}
+			} catch (InputMismatchException e){
+				System.out.println("No has introducido un numero. \nPorfavor introduzca un numero");
+				menu(); 
 			}
-			else {
-				System.out.println("Es un " +miRectangulo.getNombre());
-			}
-			System.out.println("El área es " +miRectangulo.getArea() +"cm\u00B2.");
-			System.out.println("El perímetro es " +miRectangulo.getPerimetro() +"cm.");		
-			break;
-		default: 
-			System.out.println("Opción inválida");
-			menu();
-		}
-
 	}
 
 }
